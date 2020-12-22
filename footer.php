08de3dbdf0d849e61ef23aa694d8d9b7796b5ec6
Toshiki-Mori2020 <toshiki0078@gmail.com>
2020-12-22 23:56:55 +0900

    ヘッダーとフッターの作成

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer id="footer">
+    <a href="top.php">©︎2020 共同開発募集.com</a>
+</footer>
---
+++ header.php
@@ -0,0 +1,14 @@
+<header id="header">
+    <a href="top.php">共同開発募集.com</a>
+    <ul id="header-nav">
+        <li class="header-nav_item">
+            <a href="submission.php">募集内容投稿</a>
+        </li>
+        <li class="header-nav_item">
+            <a href="membership-registration.php">募集内容投稿</a>
+        </li>
+        <li class="header-nav_item">
+            <a href="login.php">ログイン</a>
+        </li>
+    </ul>
+</header>
