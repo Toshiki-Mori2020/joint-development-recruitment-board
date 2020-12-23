b2f6e698a87e84b8838a5f97acb517f1fc4c313d
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2020-12-23 23:27:58 +0900

    ページ詳細の作成

--- top.php
+++ top.php
@@ -5,10 +5,29 @@
     <meta charset="UTF-8">
     <title>共同開発募集.com</title>
     <link rel="stylesheet" type="text/css" href="base.css">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 
 <body>
+    <div class="wrapper">
+        <div class="post_container">
+            <h1>作成内容</h1>
+            <p>・フロント1名　バック2名</p>
+            <p>・使用予定の言語JS HTML CSS PHP</p>
+            <p>・学生エンジニア募集！</p>
+            <div class="post_contents">
+                <div class="post_button1">
+                    <input type="submit" value="詳細ページ" name="submit" id="post_button_detail">
+                </div>
+                <div class="post_button2">
+                    <input type="submit" value="応募ボタン" name="submit" id="post_button_detail">
+                </div>
+                <div class="post_count">
+                    <p>応募数</p>
+                    <p>1</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </body>
