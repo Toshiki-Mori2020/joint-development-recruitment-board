1abfb576290d95673c1006c87625b34dcf33da8c
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2020-12-27 14:55:12 +0900

    マイページ画面作成

--- mypage.php
+++ mypage.php
@@ -5,10 +5,75 @@
     <meta charset="UTF-8">
     <title>共同開発募集.com</title>
     <link rel="stylesheet" type="text/css" href="base.css">
+    <link rel="stylesheet" type="text/css" href="mypage.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 
 <body>
+    <div class="wrapper">
+        <div class="title">
+            <h1>マイページ</h1>
+        </div>
+        <div class="mypage_content">
+            <div class="profile">
+                <p>名前:</p>
+                <p>ニックネーム:</p>
+                <p>使える言語1:</p>
+                <p>使える言語2:</p>
+                <p>使える言語3:</p>
+                <p>使える言語4:</p>
+                <p>使える言語5:</p>
+                <p>メールアドレス:</p>
+                <p>パスワード:</p>
+                <input type="submit" value="内容修正" name="submit" id="mypage_button_detail">
+            </div>
+
+            <div class="recruited_Projects">
+                <div class="post_container">
+                    <div class="post_box">
+                        <h1>作成内容</h1>
+                    </div>
+                    <p class="post_detail">・フロント1名　バック2名</p>
+                    <p class="post_detail">・使用予定の言語JS HTML CSS PHP</p>
+                    <p class="post_detail">・学生エンジニア募集！</p>
+                    <div class="post_contents">
+                        <div class="post_button">
+                            <input type="submit" value="詳細ページ" name="submit" id="post_button_detail">
+                        </div>
+                        <div class="post_button">
+                            <input type="submit" value="応募ボタン" name="submit" id="post_button_detail">
+                        </div>
+                        <div class="post_count">
+                            <p>応募数</p>
+                            <p class="count_box">1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="applied_Projects">
+                <div class="post_container">
+                    <div class="post_box">
+                        <h1>作成内容</h1>
+                    </div>
+                    <p class="post_detail">・フロント1名　バック2名</p>
+                    <p class="post_detail">・使用予定の言語JS HTML CSS PHP</p>
+                    <p class="post_detail">・学生エンジニア募集！</p>
+                    <div class="post_contents">
+                        <div class="post_button">
+                            <input type="submit" value="詳細ページ" name="submit" id="post_button_detail">
+                        </div>
+                        <div class="post_button">
+                            <input type="submit" value="応募ボタン" name="submit" id="post_button_detail">
+                        </div>
+                        <div class="post_count">
+                            <p>応募数</p>
+                            <p class="count_box">1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 </body>
 
