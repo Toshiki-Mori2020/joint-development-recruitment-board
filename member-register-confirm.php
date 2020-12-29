1911c790ec1dbdc5753df918059654f3762910b7
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2020-12-29 13:05:10 +0900

    会員登録確認画面の制作とCSSの調整

--- member-register-confirm.php
+++ member-register-confirm.php
@@ -4,12 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <title>共同開発募集.com</title>
-    <link rel="stylesheet" type="text/css" href="base.css">
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+    <link rel="stylesheet" type="text/css" href="css/member.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 
 <body>
-
+    <div class="wrapper">
+        <div class="title">
+            <h1>会員登録確認</h1>
+        </div>
+        <div class="member-register_content">
+            <div class="profile">
+                <p>名前:</p>
+                <p>ニックネーム:</p>
+                <p>使える言語1:</p>
+                <p>使える言語2:</p>
+                <p>使える言語3:</p>
+                <p>使える言語4:</p>
+                <p>使える言語5:</p>
+                <p>メールアドレス:</p>
+                <p>パスワード:</p>
+                <div class="member_button">
+                    <input type="submit" value="修正画面" name="submit" class="member_register_button">
+                    <input type="submit" value="登録" name="submit" class="member_register_button">
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
--- member-register.php
+++ member-register.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>共同開発募集.com</title>
     <link rel="stylesheet" type="text/css" href="css/base.css">
-    <link rel="stylesheet" type="text/css" href="css/member-register.css">
+    <link rel="stylesheet" type="text/css" href="css/member.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 
@@ -17,12 +17,12 @@
         <div class="member-register_content">
             <div class="profile">
                 <p>名前:<input type="text" name="name" id="name"><span class="red">必須</span></p>
-                <p>ニックネーム:<input type="text" name="nickname" id="nickname"><span class="red">必須</span></p>
-                <p>使える言語1:<input type="text" name="language1" id="language"><span class="red">必須</span></p>
-                <p>使える言語2:<input type="text" name="language2" id="language"></p>
-                <p>使える言語3:<input type="text" name="language3" id="language"></p>
-                <p>使える言語4:<input type="text" name="language4" id="language"></p>
-                <p>使える言語5:<input type="text" name="language5" id="language"></p>
+                <p>ニックネーム:<input type="text" placeholder="※こちらの項目は他のユーザーが確認できます" name="nickname" id="nickname"><span class="red">必須</span></p>
+                <p>使える言語1:<input type="text" placeholder="※こちらの項目は他のユーザーが確認できます" name="language1" id="language"><span class="red">必須</span></p>
+                <p>使える言語2:<input type="text" placeholder="※こちらの項目は他のユーザーが確認できます" name="language2" id="language"></p>
+                <p>使える言語3:<input type="text" placeholder="※こちらの項目は他のユーザーが確認できます" name="language3" id="language"></p>
+                <p>使える言語4:<input type="text" placeholder="※こちらの項目は他のユーザーが確認できます" name="language4" id="language"></p>
+                <p>使える言語5:<input type="text" placeholder="※こちらの項目は他のユーザーが確認できます" name="language5" id="language"></p>
                 <p>メールアドレス:<input type="text" name="mail" id="mail"><span class="red">必須</span></p>
                 <p>パスワード:<input type="text" name="password" id="password"><span class="red">必須</span></p>
                 <p>※使える言語は担当したい領域で使える言語を記載してください</p>
