fe19da527385927b7881b49ed3930d7168ebae2a
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2021-01-01 17:47:19 +0900

    応募詳細画面の作成

---
+++ recruitment-detail.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>共同開発募集.com</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+    <link rel="stylesheet" type="text/css" href="css/recruit.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="member-register_content">
+            <div class="recruit_container">
+                <div class="post_box">
+                    <h1>作成内容</h1>
+                </div>
+                <p>・フロント1名　バック2名</p>
+                <p>・使用予定の言語JS HTML CSS PHP</p>
+                <p>・学生エンジニア募集！</p>
+                <div class="recruit_button">
+                    <input type="submit" value="修正" name="submit" class="recruit_register_button">
+                    <input type="submit" value="募集終了" name="submit" class="recruit_register_button">
+                </div>
+            </div>
+        </div>
+        <div class="message">
+            <p>ニックネーム：〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇</p>
+            <p>ニックネーム：××××××××××</p>
+        </div>
+        <div class="message_input_column">
+            <p>メッセージ入力</p>
+            <textarea name="comment" rows="6" cols="60"></textarea>
+            <input type="submit" value="メッセージの投稿" name="message">
+        </div>
+
+
+    </div>
+</body>
+
+</html>
