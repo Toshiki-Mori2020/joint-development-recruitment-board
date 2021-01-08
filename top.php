5a09231f069bed05fc4ded08b8e6116ed6ce5edb
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2021-01-08 22:13:01 +0900

    DB接続したものを表示

--- top.php
+++ top.php
@@ -15,8 +15,11 @@ try {
     $number_of_applicants = $_POST['number_of_applicants'];
     $language = $_POST['language'];
     $a_few_words = $_POST['a_few_words'];
+
+    $result_list = $pdo->query('SELECT * FROM recruitment');
 } catch (PDOException $e) {
     exit('データベースに接続できませんでした。' . $e->getMessage());
+    die();
 }
 
 ?>
@@ -34,26 +37,28 @@ try {
 <body>
     <?php include("header.php"); ?>
     <div class="wrapper">
-        <div class="post_container">
-            <div class="post_box">
-                <h1>作成内容</h1>
+        <?php foreach ($result_list as $row) : ?>
+            <div class="post_container">
+                <div class="post_box">
+                    <h1><?php "{$row['contents']}" ?></h1>
+                </div>
+                <p class="post_detail">・<?php "{$row['number_of_applicants']}" ?></p>
+                <p class="post_detail">・<?php "{$row['language']}" ?></p>
+                <p class="post_detail">・<?php "{$row['a_few_words']}" ?></p>
+                <div class="post_contents">
+                    <div class="post_button">
+                        <input type="submit" value="詳細ページ" name="submit" id="post_button_detail">
+                    </div>
+                    <div class="post_button">
+                        <input type="submit" value="応募ボタン" name="submit" id="post_button_detail">
+                    </div>
+                    <div class="post_count">
+                        <p>応募数</p>
+                        <p class="count_box">1</p>
+                    </div>
+                </div>
             </div>
-            <p class="post_detail">・フロント1名　バック2名</p>
-            <p class="post_detail">・使用予定の言語JS HTML CSS PHP</p>
-            <p class="post_detail">・学生エンジニア募集！</p>
-            <div class="post_contents">
-                <div class="post_button">
-                    <input type="submit" value="詳細ページ" name="submit" id="post_button_detail">
-                </div>
-                <div class="post_button">
-                    <input type="submit" value="応募ボタン" name="submit" id="post_button_detail">
-                </div>
-                <div class="post_count">
-                    <p>応募数</p>
-                    <p class="count_box">1</p>
-                </div>
-            </div>
-        </div>
+        <?php endforeach; ?>
 
         <div class="post_container">
             <div class="post_box">
