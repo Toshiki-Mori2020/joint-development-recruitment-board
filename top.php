caae26f0e1966df20397b05847a50f31d74a501a
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2021-01-12 23:15:11 +0900

    ひとこと枠の文字数で改行処理

--- top.php
+++ top.php
@@ -2,25 +2,24 @@
 
 require_once("dbconnect.php");
 
-function mb_wordwrap($str, $width = 50, $break = "\n")
+//バイト数の指定から文字数で決める
+
+function mb_wordwrap($str, $width = 14, $break = "\n")
 {
-    $w = mb_strwidth($str, "eucJP-win");
+    $w = mb_strwidth($str, "UTF-8");
     $arr = array();
     if ($w < $width) {
         return $str;
     }
     while ($w > $width) {
-        $trimStr = mb_strimwidth($str, 0, $width, "", "eucJP-win");
+        $trimStr = mb_strimwidth($str, 0, $width, "", "UTF-8");
         $arr[] = $trimStr;
         $str = preg_replace("/^$trimStr/", "", $str);
-        $w = mb_strwidth($str, "eucJP-win");
+        $w = mb_strwidth($str, "UTF-8");
     }
     $arr[] = $str;
     return implode($break, $arr);
 }
-
-
-$str = $row['a_few_words'];
 
 ?>
 
@@ -45,7 +44,8 @@ $str = $row['a_few_words'];
                 <p class="post_detail"><?php echo "募集人数：" . "{$row['number_of_applicants']}" . "名" ?></p>
                 <p class="post_detail"><?php echo "使いたい言語：" . "{$row['language']}" ?></p>
                 <p class="post_detail">-------------------------------------</p>
-                <p class="post_detail"><?php echo "ひとこと：" . mb_wordwrap($str, 7, '<br />'); ?></p>
+                <?php $str = $row['a_few_words']; ?>
+                <p class="post_detail"><?php echo "ひとこと：" . mb_wordwrap($str, 14, '<br />'); ?></p>
                 <p class="post_detail">-------------------------------------</p>
                 <div class="post_contents">
                     <div class="post_button">
