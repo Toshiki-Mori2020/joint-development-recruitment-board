a250511b12ec98c7664c654c52339bd8832b2ee4
Toshiki-Mori2020 <[toshiki0078@gmail.com]>
2021-01-30 20:26:24 +0900

    応募ボタンを押した際にデータベースへ登録

--- top.php
+++ top.php
@@ -15,6 +15,24 @@ if ($_GET['apply_hid'] == "1") {
     $disabled = "disabled";
 }
 
+$sql2 = "INSERT INTO apply(member_id, recruitment_id) VALUES (?, ?)";
+
+if (!empty($_POST['apply_hid'])) {
+    try {
+        $sql = 'INSERT INTO apply(member_id, recruitment_id) VALUES (?, ?)';
+        $stmt = $dbh->prepare($sql);
+
+        $stmt->bindParam(1, $_POST['member_id'], PDO::PARAM_INT);
+        $stmt->bindParam(1, $_POST['recruitment_id'], PDO::PARAM_INT);
+        $stmt->execute();
+
+        header('location: http://localhost/portfolio/joint-development-recruitment-board/top.php');
+        exit();
+    } catch (PDOException $e) {
+        echo 'データベースにアクセスできません' . $e->getMessage();
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -45,8 +63,9 @@ if ($_GET['apply_hid'] == "1") {
                         <button type="button" onclick="location.href='recruitment-detail.php'" id="post_button_detail">詳細ページ</button>
                     </div>
                     <div class="post_button">
-                        <input type="hidden" name="apply_hid" value="1">
-                        <input type="submit" value="応募ボタン" name="apply_button" id="post_button_detail" <?php $disabled ?>>
+                        <form action="top.php" method="POST">
+                            <input type="hidden" name="apply_hid" value="1">
+                            <input type="submit" value="応募ボタン" name="apply_button" id="post_button_detail" <?php $disabled ?>>
                     </div>
                     <div class="post_count">
                         <p>応募数</p>
@@ -61,3 +80,11 @@ if ($_GET['apply_hid'] == "1") {
 </body>
 
 </html>
+
+
+<div class="button" style="display:inline-block">
+    <a href="https://tan-taka.com/diver/contact" style="color:#F8FFF8;background:#3CA44F;"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+</div>
+<div class="button" style="display:inline-block">
+    <a href="https://tan-taka.com/diver/" style="color:#F8FFF8;background:#3CA44F;">お問い合わせ<br>043-309-8529</a>
+</div>
